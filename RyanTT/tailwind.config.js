77f34871a827455ee090f8f0c937bfe685fab3cd
Ryan <ryantorzynski@gmail.com>
2021-12-20 19:42:53 +0100

    Added Inter var default font

--- RyanTT/tailwind.config.js
+++ RyanTT/tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["**/*.{html,razor,razor.cs}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+          sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   plugins: [],
 }
